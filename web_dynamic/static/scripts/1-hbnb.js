50a14e6baed3cb578e794c20590e5b3be8e0c570
Adnangad <gardobuyaadnan@gmail.com>
2024-05-03 09:33:10 -0700

    2-checkboxes

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,14 @@
-$(document).ready(function(){
-	ls = {};
-	$('.checkBox').change(function(){
-		const amId = $(this).data('id');
-		const amnm = $(this).data('name');
-		if ($(this).is(':checked')) {
-			ls[amId] = amnm;
-		}
-		else {
-			delete ls[amId];
-		}
-		$('.amenities h4').empty();
-		for (const key in ls) {
-			$('.amenities h4').append('<li>' + ls[key] + '</li>');
-		}
-	});
+$(document).ready(function () {
+  const ls = {};
+  $('.checkBox').change(function () {
+    const amId = $(this).data('id');
+    const amnm = $(this).data('name');
+    if ($(this).is(':checked')) {
+      ls[amId] = amnm;
+    } else {
+      delete ls[amId];
+    }
+    const amenitiesList = Object.values(ls).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
 });
