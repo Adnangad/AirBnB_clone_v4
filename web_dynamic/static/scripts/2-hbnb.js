840ae7618c18f39f29ac2f73628fa7765dfbd819
Adnangad <gardobuyaadnan@gmail.com>
2024-05-04 04:41:41 -0700

    3-status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  const ls = {};
+  $('.checkBox').change(function () {
+    const amId = $(this).data('id');
+    const amnm = $(this).data('name');
+    if ($(this).is(':checked')) {
+      ls[amId] = amnm;
+    } else {
+      delete ls[amId];
+    }
+    const amenitiesList = Object.values(ls).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
+});
+$(document).ready(function () {
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    method: 'GET',
+    success: function (response) {
+      if (response.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    }
+  });
+});
